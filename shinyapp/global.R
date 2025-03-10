422286b5039e7d08aa7f1273995d7fe8ade0174f
thekangaroofactory <67638275+thekangaroofactory@users.noreply.github.com>
2025-03-10 19:05:26 +0100

    add DEBUG

--- shinyapp/global.R
+++ shinyapp/global.R
@@ -5,6 +5,10 @@ library(shiny)
 library(bslib)
 
 
+# -- environment
+DEBUG <- as.logical(Sys.getenv("DEBUG"))
+
+
 # -- Init & source code
 for(nm in list.files("R", pattern = ".R", full.names = T, recursive = T))
   source(nm)
