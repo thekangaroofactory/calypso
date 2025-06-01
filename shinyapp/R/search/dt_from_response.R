ba9f563205ff951db96d426ef4f3acad21beff98
thekangaroofactory <67638275+thekangaroofactory@users.noreply.github.com>
2025-06-01 10:16:11 +0200

    cleanup dataframe names

--- shinyapp/R/search/dt_from_response.R
+++ shinyapp/R/search/dt_from_response.R
@@ -7,23 +7,23 @@ dt_from_response <- function(x){
     
     data.frame(
       "siren" = x$uniteLegale$siren,
-      "dénomination" = x$uniteLegale$periodesUniteLegale[x$uniteLegale$nombrePeriodesUniteLegale, "denominationUniteLegale"],
+      "denomination" = x$uniteLegale$periodesUniteLegale[x$uniteLegale$nombrePeriodesUniteLegale, "denominationUniteLegale"],
       "nom" = x$uniteLegale$periodesUniteLegale[x$uniteLegale$nombrePeriodesUniteLegale, "nomUniteLegale"],
       "prenom" = x$uniteLegale$prenom1UniteLegale,
       "creation" = x$uniteLegale$dateCreationUniteLegale,
-      "mise à jour" = x$uniteLegale$dateDernierTraitementUniteLegale,
-      "activité" = x$uniteLegale$periodesUniteLegale[x$uniteLegale$nombrePeriodesUniteLegale, "activitePrincipaleUniteLegale"])
+      "mise_a_jour" = x$uniteLegale$dateDernierTraitementUniteLegale,
+      "activite" = x$uniteLegale$periodesUniteLegale[x$uniteLegale$nombrePeriodesUniteLegale, "activitePrincipaleUniteLegale"])
   
   else if("unitesLegales" %in% names(x))
     
     data.frame(
       "siren" = x$unitesLegales$siren,
-      "dénomination" = unlist(lapply(x$unitesLegales$periodesUniteLegale, function(x) x$denominationUniteLegale)),
+      "denomination" = unlist(lapply(x$unitesLegales$periodesUniteLegale, function(x) x$denominationUniteLegale)),
       "nom" = unlist(lapply(x$unitesLegales$periodesUniteLegale, function(x) x$nomUniteLegale)),
       "prenom" = x$unitesLegales$prenom1UniteLegale,
       "creation" = x$unitesLegales$dateCreationUniteLegale,
-      "mise à jour" = x$unitesLegales$dateDernierTraitementUniteLegale,
-      "activité" = unlist(lapply(x$unitesLegales$periodesUniteLegale, function(x) x$activitePrincipaleUniteLegale)))
+      "mise_a_jour" = x$unitesLegales$dateDernierTraitementUniteLegale,
+      "activite" = unlist(lapply(x$unitesLegales$periodesUniteLegale, function(x) x$activitePrincipaleUniteLegale)))
   
   
   else if("etablissement" %in% names(x))
@@ -34,7 +34,7 @@ dt_from_response <- function(x){
       "nom" = x$etablissement$uniteLegale$nomUniteLegale,
       "prenom" = x$etablissement$uniteLegale$prenom1UniteLegale,
       "creation" = x$etablissement$dateCreationEtablissement,
-      "mise à jour" = x$etablissement$dateDernierTraitementEtablissement,
-      "activité" = x$etablissement$uniteLegale$activitePrincipaleUniteLegale)
+      "mise_a_jour" = x$etablissement$dateDernierTraitementEtablissement,
+      "activite" = x$etablissement$uniteLegale$activitePrincipaleUniteLegale)
   
 }
