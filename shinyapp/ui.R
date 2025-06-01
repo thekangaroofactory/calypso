0e5e1782fd17710594580a096dfebc5a795d5a0b
thekangaroofactory <67638275+thekangaroofactory@users.noreply.github.com>
2025-06-01 09:15:31 +0200

    Fixed #10

--- shinyapp/ui.R
+++ shinyapp/ui.R
@@ -8,14 +8,14 @@ page_navbar(
   
   # -- footer
   fillable = TRUE,
-  footer = p(style = "font-size:9pt;margin-top:20px;color:#FFF;", "© 2025 Philippe Peret"),
+  footer = p(style = "font-size:9pt;margin-top:20px;margin-left:20px;color:#FFF;", "© 2025 Philippe Peret"),
   
   # -- header / css
   header = tags$link(rel = "stylesheet", type = "text/css", href = "./css/base.css"),
   
   # -- theme
   theme = bs_theme(
-    primary = "orange",
+    primary = "#02587c",
     secondary = "#ececec",
     base_font = font_google("Quicksand")),
   
@@ -26,12 +26,22 @@ page_navbar(
   nav_panel(title = "Rechercher",
             class = "p-5",
             
-            # -- content
-            p("Calypso effectue des recherches dans le répertoire SIRENE."),
-            p("Cette fonctionalité est basée sur l'appel en temps réel à l'API Sirene publiée par l'INSEE.", br(),
-            "La recherche est limitée à 20 résultats. Plus d'information [ici]."),
-            
-            search_health_ui("search"),
+            layout_column_wrap(
+              
+              # -- card
+              card(
+                card_header("Présentation"),
+                
+                # -- content
+                p("Calypso effectue des recherches dans le répertoire SIRENE."),
+                p("Cette fonctionalité est basée sur l'appel en temps réel à", 
+                  tags$a("l'API",
+                         target = "_blank",
+                         href = "https://www.data.gouv.fr/fr/dataservices/api-sirene-open-data/"),
+                  "Sirene publiée par l'INSEE.", br(),
+                  "La recherche est limitée à 20 résultats.")),
+              
+              search_health_ui("search")),
             
             search_ui("search"),
             
@@ -39,18 +49,31 @@ page_navbar(
   
   
   # -- explore
-  nav_panel(title = "Explorer",
-            class = "p-5",
-            
-            # -- content
-            p("Something goes here too")),
+  # nav_panel(title = "Explorer",
+  #           class = "p-5",
+  #           
+  #           # -- content
+  #           p("Something goes here too")),
   
   
   # -- about
   nav_panel(title = "A propos",
             class = "p-5",
             
-            # -- content
-            p("Calypso explore le répertoire SIRENE."))
+            card(
+              fill = FALSE,
+              card_header("A propos"),
+              
+              # -- content
+              p("Calypso explore le répertoire SIRENE.", br(),
+                "Toutes les données viennent de l'API mise à disposition par l'INSEE."),
+              
+              p("La réutilisation est publiée sur la platforme data.gouv.fr.", br(),
+                tags$a("data.gouv.f", target = "_blank", href = "https://www.data.gouv.fr/fr/organizations/philippe-peret/reuses/"))),
+            
+            card(
+              fill = FALSE,
+              card_header("Contact"),
+              div(icon("linkedin"), tags$a("LinkedIn", target = "_blank", href = "https://www.linkedin.com/in/philippeperet/"))))
   
 )
